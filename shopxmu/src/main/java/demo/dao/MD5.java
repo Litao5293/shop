83899823bd8252d3205de9bffa22539b0af5b592
979xin <1017110351@qq.com>
2017-11-01 13:54:20 +0800

    md5

--- shopxmu/src/main/java/demo/dao/MD5.java
+++ shopxmu/src/main/java/demo/dao/MD5.java
@@ -7,10 +7,10 @@ import java.security.NoSuchAlgorithmException;
 import sun.misc.BASE64Encoder;
 
 public class MD5 {
-	public String EncoderMyMd5(String str)throws NoSuchAlgorithmException,UnsupportedEncodingException{
+	public String EncoderMyMd5(String pwd1)throws NoSuchAlgorithmException,UnsupportedEncodingException{
 		MessageDigest md5 = MessageDigest.getInstance("MD5");//关键字加密法
 		BASE64Encoder base64en = new BASE64Encoder();//使用BASE64格式进行编码
-		String newstr = base64en.encode(md5.digest(str.getBytes("utf-8")));
+		String newstr = base64en.encode(md5.digest(pwd1.getBytes("utf-8")));
 	
 		return newstr;
 	}
